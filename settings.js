f921b02a3e0545aa142f926c63f0a4e346a9cf92
Knightbot-MD <mcfallenlungu@gmail.com>
2025-04-09 15:06:06 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Shining Of Ages",
+  botOwner: 'McFallen Lungu', // Your name
+  ownerNumber: '+260765609022', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
